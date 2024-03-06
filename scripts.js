4746a608c26f5d203ea8358fbc88eb92979ec1da
Harrison <109526528+HuiHarrison@users.noreply.github.com>
2024-03-06 21:11:17 +0800

    Add displayLibrary() to Display Books in myLibrary

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const authorInput = document.querySelector("#author-input");
 const pagesInput = document.querySelector("#pages-input");
 const readInput = document.querySelector("#read-input");
 const submitBtn = document.querySelector("button[type='submit']");
+const container = document.querySelector(".container");
 
 const myLibrary = [];
 
@@ -45,4 +46,20 @@ submitBtn.addEventListener("click", (event) => {
     pagesInput.value = "";
     readInput.checked = false;
     dialog.close();
+    displayLibrary();
 })
+
+function displayLibrary() {
+    container.innerHTML = "";
+    for (const book of myLibrary) {
+        container.innerHTML += `
+            <div class="card">
+                <div class="book-title">${book.title}</div>
+                <div class="book-author">${book.author}</div>
+                <div class="book-pages">${book.pages} pages</div>
+                <button type="button">${book.read ? "Read" : "Not Read"}</button>
+                <button class="remove-button" type="button">Remove</button>
+            </div>
+        `;
+    }
+}
